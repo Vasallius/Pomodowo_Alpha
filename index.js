490a0d2b81014592748be6a9d11fd9e62c87b1ff
jotan7 <jotan7@up.edu.ph>
2021-01-23 00:32:09 +0800

    ✨ add functional add task

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-// $(document).ready(function () {
-//   $("#MyModal").modal();
+// 📚 MODAL
+// document.querySelector("#click").addEventListener("click", function () {
+//   $("#taskModal").modal("show");
 // });
 
-document.querySelector("#click").addEventListener("click", function () {
-  $("#taskModal").modal("show");
-});
-
-// document.querySelector(".close-btn").addEventListener("click", function () {
-//   $("#exampleModal").modal("hide");
+// $(".close-btn").click(function () {
+//   $("#taskModal").modal("hide");
 // });
 
-$(".close-btn").click(function () {
-  $("#taskModal").modal("hide");
+$("#submit-task").keyup(function (e) {
+  if (e.keyCode === 13) {
+    const task = $(this).val();
+    console.log(task);
+    // Add task to the list
+    $(".list-group").append(
+      '<li class="list-group-item my-2 border-top">' + task + "</li>"
+    );
+    $(this).val("");
+  }
 });
+
+// 📚 Clear Input
+// function Clear() {
+//   $("#submit-task").each(function () {
+//     $(this).val("");
+//     // x = 1;
+//   });
+//   //   $(".#submit-task").focus();
+// }
