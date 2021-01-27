85277d5205600cd3c6e4db089e7871bebd0d9e5a
jotan7 <jotan7@up.edu.ph>
2021-01-27 13:45:27 +0800

    ✨ add persistent storage through local storage

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-// 📚 MODAL
-// document.querySelector("#click").addEventListener("click", function () {
-//   $("#taskModal").modal("show");
-// });
-
-// $(".close-btn").click(function () {
-//   $("#taskModal").modal("hide");
-// });
 const tasks = [];
+let task_segments = [];
+var theWheel;
+
+$(document).ready(getTasks);
+$(document).ready(loadWheel);
+
 $("#submit-task").keyup(function (e) {
   // After user presses "Enter"
 
   if (e.keyCode === 13) {
+    console.log("===Adding task====");
     const task = $(this).val();
-    console.log(task);
 
     // Add task to the list with clear button
 
@@ -22,11 +20,11 @@ $("#submit-task").keyup(function (e) {
         '<i class="fas fa-times ml-auto"></i></li></div>'
     );
 
+    storeTaskInLocalStorage(task);
+
     // Add to wheel
-
-    tasks.push(task);
-
     function addSegment() {
+      console.log("===Adding segment to wheel===");
       theWheel.addSegment(
         {
           text: task,
@@ -38,11 +36,12 @@ $("#submit-task").keyup(function (e) {
     }
 
     addSegment();
-
+    console.log("===Segment has been added===");
     // Remove task button
 
     $("i").click(function () {
-      console.log($(this).parent().hasClass("list-group-item"));
+      const task = $(this).parent()[0].innerText;
+      removeTaskFromLocalStorage(task);
       $(this).parent().remove();
       deleteSegment();
     });
@@ -62,39 +61,173 @@ $("#submit-task").keyup(function (e) {
   }
 });
 
+function getTasks() {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.forEach(function (task) {
+    $(".list-group").append(
+      '<div class="col-xs-2 col-md-6"><li class="list-group-item my-2 border-top d-flex justify-content-between align-items-center">' +
+        task +
+        '<i class="fas fa-times ml-auto"></i></li></div>'
+    );
+    let segment = { fillStyle: "#eae56f", text: task };
+    task_segments.push(segment);
+  });
+}
+
+function loadWheel() {
+  let segments;
+  if (localStorage.getItem("tasks_segments") === null) {
+    segments = [];
+  } else {
+    segments = JSON.parse(localStorage.getItem("tasks_segments"));
+  }
+
+  // console.log(JSON.parse(localStorage.getItem("tasks_segments")));
+  console.log(segments.length);
+  theWheel = new Winwheel({
+    numSegments: segments.length,
+    // segments: [
+    //   { fillStyle: "#eae56f", text: "Task 1" },
+    //   { fillStyle: "#89f26e", text: "Task 2" },
+    //   { fillStyle: "#7de6ef", text: "Task 3" },
+    //   { fillStyle: "#e7706f", text: "Task 4" },
+    // ],
+    segments: task_segments,
+    fillStyle: "#e7706f",
+    textAlignment: "center",
+    responsive: true,
+    // outerRadius: 150,
+    lineWidth: 3,
+    rotationAngle: 45,
+    // Note animation properties passed in constructor parameters.
+    animation: {
+      type: "spinToStop", // Type of animation.
+      duration: 5, // How long the animation is to take in seconds.
+      spins: 8, // The number of complete 360 degree rotations the wheel is to do.
+      easing: "Power4.easeOut",
+      // stopAngle: 45,
+      callbackFinished: alertPrize,
+    },
+
+    // Turn pointer guide on.
+    // pointerGuide: {
+    //   display: true,
+    //   strokeStyle: "red",
+    //   lineWidth: 3,
+    // },
+  });
+}
+
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  let tasks_segments;
+  if (localStorage.getItem("tasks_segments") === null) {
+    task_segments = [];
+  } else {
+    task_segments = JSON.parse(localStorage.getItem("tasks_segments"));
+  }
+
+  let segment = { fillStyle: "#eae56f", text: task };
+  tasks.push(task);
+  task_segments.push(segment);
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  localStorage.setItem("tasks_segments", JSON.stringify(task_segments));
+}
+
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  let tasks_segments;
+  if (localStorage.getItem("tasks_segments") === null) {
+    task_segments = [];
+  } else {
+    task_segments = JSON.parse(localStorage.getItem("tasks_segments"));
+  }
+
+  let segment = { fillStyle: "#eae56f", text: task };
+  tasks.push(task);
+  task_segments.push(segment);
+
+  tasks.forEach(function (task, index) {
+    if (taskItem === task) {
+      tasks.splice(index, 1);
+    }
+  });
+
+  task_segments.forEach(function (segment, index) {
+    if (segment.text === task) {
+      task_segments.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
 // Wheel Construction
 
-let theWheel = new Winwheel({
-  numSegments: 4,
-  segments: [
-    { fillStyle: "#eae56f", text: "Task 1" },
-    { fillStyle: "#89f26e", text: "Task 2" },
-    { fillStyle: "#7de6ef", text: "Task 3" },
-    { fillStyle: "#e7706f", text: "Task 4" },
-  ],
-  fillStyle: "#e7706f",
-  textAlignment: "center",
-  responsive: true,
-  // outerRadius: 150,
-  lineWidth: 3,
-  rotationAngle: 45,
-  // Note animation properties passed in constructor parameters.
-  animation: {
-    type: "spinToStop", // Type of animation.
-    duration: 5, // How long the animation is to take in seconds.
-    spins: 8, // The number of complete 360 degree rotations the wheel is to do.
-    easing: "Power4.easeOut",
-    stopAngle: 45,
-    callbackFinished: alertPrize,
-  },
+// task_segments = [];
+// let tasks2;
+// if (localStorage.getItem("tasks") === null) {
+//   tasks2 = [];
+// } else {
+//   tasks2 = JSON.parse(localStorage.getItem("tasks"));
+// }
 
-  // Turn pointer guide on.
-  // pointerGuide: {
-  //   display: true,
-  //   strokeStyle: "red",
-  //   lineWidth: 3,
-  // },
-});
+// tasks2.forEach(function (task) {
+//   let segment = { fillStyle: "#eae56f", text: task };
+//   task_segments.push(segment);
+// });
+
+// let theWheel = new Winwheel({
+//   numSegments: 4,
+//   // segments: [
+//   //   { fillStyle: "#eae56f", text: "Task 1" },
+//   //   { fillStyle: "#89f26e", text: "Task 2" },
+//   //   { fillStyle: "#7de6ef", text: "Task 3" },
+//   //   { fillStyle: "#e7706f", text: "Task 4" },
+//   // ],
+//   segments: task_segments,
+//   fillStyle: "#e7706f",
+//   textAlignment: "center",
+//   responsive: true,
+//   // outerRadius: 150,
+//   lineWidth: 3,
+//   rotationAngle: 45,
+//   // Note animation properties passed in constructor parameters.
+//   animation: {
+//     type: "spinToStop", // Type of animation.
+//     duration: 5, // How long the animation is to take in seconds.
+//     spins: 8, // The number of complete 360 degree rotations the wheel is to do.
+//     easing: "Power4.easeOut",
+//     // stopAngle: 45,
+//     callbackFinished: alertPrize,
+//   },
+
+//   // Turn pointer guide on.
+//   // pointerGuide: {
+//   //   display: true,
+//   //   strokeStyle: "red",
+//   //   lineWidth: 3,
+//   // },
+// });
 
 function alertPrize() {
   // Call getIndicatedSegment() function to return pointer to the segment pointed to on wheel.
@@ -102,11 +235,9 @@ function alertPrize() {
 
   // Basic alert of the segment text which is the prize name.
   alert("You have won " + winningSegment.text + "!");
-  // theWheel.draw();
-  // theWheel.startAnimation();
-}
 
-function resetWheel() {
+  // Reset the wheel
+
   theWheel.stopAnimation(false); // Stop the animation, false as param so does not call callback function.
   theWheel.rotationAngle = 0; // Re-set the wheel angle to 0 degrees.
   theWheel.draw(); // Call draw to render changes to the wheel.
@@ -118,7 +249,3 @@ $("#test-btn").click(function () {
   console.log("i got clicked");
   theWheel.startAnimation();
 });
-
-// Resets wheel so it can be spun again
-
-$("#reset").click(resetWheel);
