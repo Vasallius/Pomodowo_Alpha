933c2b910accc2e08e73dc7b4fd7b3b59a111d07
jotan7 <jotan7@up.edu.ph>
2021-02-05 23:44:13 +0800

    FEAT: fixed task UI

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      width: {
+        logo: "50px",
+        task_id: "25px",
+      },
+      height: {
+        logo: "50px",
+        nav: "65px",
+        task_id: "25px",
+      },
+      colors: {
+        Black_Substitute: "#06112D",
+        Main_Orange: "#FF9575",
+        Light_Orange: "#FFE4DC",
+        White: "#FFFFFF",
+        Background: "#FBF8F1",
+        Light_Yellow_Accent: "#F8F1E4",
+      },
+    },
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
