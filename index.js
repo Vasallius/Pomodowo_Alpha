58d73e9a43cbc34734ac42392a72cc4856424d1c
jotan7 <jotan7@up.edu.ph>
2021-02-07 21:47:06 +0800

    modal drop down

--- index.js
+++ index.js
@@ -30,13 +30,14 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
 btn.onclick = function () {
+  gsap.fromTo(modal, { top: "-300px", opacity: 0 }, { top: "0", opacity: "1" });
   modal.style.display = "block";
 };
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
+// span.onclick = function () {
+//   modal.style.display = "none";
+// };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
